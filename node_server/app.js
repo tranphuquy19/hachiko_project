15dc28c79a3906fa042b5dcbfa32d0ea03907968
Quy <tranphuquy19@gmail.com>
2020-01-19 02:14:10 +0700

    add zh js

--- node_server/app.js
+++ node_server/app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // i18n config
 i18n.configure({
-  locales: ['vi', 'en'],
+  locales: ['vi', 'en', 'ja', 'zh'],
   defaultLocale: 'vi',
   queryParameter: 'lang',
   directory: path.join(__dirname, 'public', 'langs'),
